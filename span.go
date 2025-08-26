81fd3ee2f804f1ba3f0ce7c103b6117f40fb26a9
chenbiao <149027310@qq.com>
2025-08-26 15:39:56 +0800

    init

--- span.go
+++ span.go
@@ -3,9 +3,7 @@ package dgotel
 import (
 	"context"
 
-	"github.com/darwinOrg/go-common/constants"
 	dgctx "github.com/darwinOrg/go-common/context"
-	dgsys "github.com/darwinOrg/go-common/sys"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 )
@@ -24,55 +22,22 @@ func DoActionWithinNewSpan(ctx context.Context, spanName string, opts []trace.Sp
 	action(ctx, span)
 }
 
-func SetSpanAttributesByDgContext(ctx *dgctx.DgContext) {
+func SetSpanAttributes(ctx *dgctx.DgContext, mp map[string]string) {
+	if len(mp) == 0 {
+		return
+	}
+
 	span := GetSpanByDgContext(ctx)
 	if span == nil {
 		return
 	}
 
 	var attrs []attribute.KeyValue
-
-	profile := dgsys.GetProfile()
-	if profile != "" {
-		attrs = append(attrs, attribute.String(constants.Profile, profile))
-	}
-	if ctx.UserId > 0 {
-		attrs = append(attrs, attribute.Int64(constants.UID, ctx.UserId))
-	}
-	if ctx.OpId > 0 {
-		attrs = append(attrs, attribute.Int64(constants.OpId, ctx.OpId))
-	}
-	if ctx.RunAs > 0 {
-		attrs = append(attrs, attribute.Int64(constants.RunAs, ctx.RunAs))
-	}
-	if ctx.Roles != "" {
-		attrs = append(attrs, attribute.String(constants.Roles, ctx.Roles))
-	}
-	if ctx.BizTypes > 0 {
-		attrs = append(attrs, attribute.Int(constants.BizTypes, ctx.BizTypes))
-	}
-	if ctx.GroupId > 0 {
-		attrs = append(attrs, attribute.Int64(constants.GroupId, ctx.GroupId))
-	}
-	if ctx.Platform != "" {
-		attrs = append(attrs, attribute.String(constants.Platform, ctx.Platform))
-	}
-	if ctx.CompanyId > 0 {
-		attrs = append(attrs, attribute.Int64(constants.CompanyId, ctx.CompanyId))
-	}
-	if ctx.Product > 0 {
-		attrs = append(attrs, attribute.Int(constants.Product, ctx.Product))
-	}
-	if len(ctx.Products) > 0 {
-		attrs = append(attrs, attribute.IntSlice(constants.Products, ctx.Products))
-	}
-	if len(ctx.DepartmentIds) > 0 {
-		attrs = append(attrs, attribute.Int64Slice(constants.Products, ctx.DepartmentIds))
+	for k, v := range mp {
+		attrs = append(attrs, attribute.String(k, v))
 	}
 
-	if len(attrs) > 0 {
-		span.SetAttributes(attrs...)
-	}
+	span.SetAttributes(attrs...)
 }
 
 func GetSpanByDgContext(ctx *dgctx.DgContext) trace.Span {
@@ -86,17 +51,4 @@ func GetSpanByDgContext(ctx *dgctx.DgContext) trace.Span {
 	}
 
 	return trace.SpanFromContext(ctx.GetInnerContext())
-}
-
-func SetSpanAttributesByMap(span trace.Span, mp map[string]string) {
-	if span == nil || len(mp) == 0 {
-		return
-	}
-
-	var attrs []attribute.KeyValue
-	for k, v := range mp {
-		attrs = append(attrs, attribute.String(k, v))
-	}
-
-	span.SetAttributes(attrs...)
 }
